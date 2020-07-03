6da6a3891b925eb1243839848f9af7cb370e5717
aashishpeepra <ironhack082327@gmail.com>
2020-07-03 14:40:34 +0530

    Added Wrapper and new Merges

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react';
 import Products from "../../Components/UI/Products/Products";
 import Customers from "../../Components/UI/Customers/Customers";
 import Category from "../../Components/UI/Categorize/Categorize";
-export default class Home extends Component {
-    state = {
 
-    }
+import "./Home.css";
+export default class Home extends Component {
+    
+    
     render() {
         return (
             <main>
-                <h1>Home</h1>
+               
+                
+                {/* <h1>Home</h1> */}
                 <h3>Trending Products</h3>
                 <Products type="listed"  />
                
@@ -18,44 +21,26 @@ export default class Home extends Component {
                 <h3>Top Picks For you</h3>
                 <Products type="listed"  />
                  <Customers/>
+                  
             </main>
 
         );
     }
 }
-// import Products from "../../Components/UI/Products/Products";
-// import Footer from "../../Components/UI/Footer/Footer";
-import Toolbar from "../../Components/Toolbar/Toolbar";
-// import SideDrawer from "../../Components/SideDrawer/SideDrawer";
-// import Backdrop from "../../Components/Backdrop/Backdrop";
+
  class Home2 extends Component {
   state = {
-    SideDrawerOpen: false,
+    
   };
 
-  drawerToggleClickHandler = () => {
-    this.setState((prevState) => {
-      return { sideDrawerOpen: !prevState.SideDrawerOpen };
-    });
-  };
-
-  backdropClickHandler = () => {
-    this.setState({ sideDrawerOpen: false });
-  };
-
+ 
   render() {
-    // let backdrop;
-    // if (this.state.sideDrawerOpen) {
-    //   backdrop = <Backdrop clicked={this.backdropClickHandler} />;
-    // }
+   
     return (
       <main style={{ height: "100%" }}>
-        {/* <h1>Home</h1>
+        <h1>Home</h1>
         <Products />
-        <Footer /> */}
-        <Toolbar clicked={this.drawerToggleClickHandler} />
-        {/* <SideDrawer show={this.state.sideDrawerOpen} />
-        {backdrop} */}
+        
       </main>
     );
   }
--- src/Containers/Products/Products.js
+++ src/Containers/Products/Products.js
@@ -10,27 +10,38 @@ export default class Products extends React.Component {
     render() {
         return (
             <div>
-                <h1>Navigation</h1>
                 <main className="Prd-input-holder">
 
                     <div class="wrapper"><ul class="main-menu">
                         
                         <li id="menu-item-16" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-16"><a href="#">Age</a>
                             <ul class="sub-menu">
-                                <li id="menu-item-7" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-7"><a href="/sample-page/">Sub Page 1</a></li>
-                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Sub Page 2</a></li>
+                                <li id="menu-item-7" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-7"><a href="/sample-page/">Infants</a></li>
+                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Age 2-6</a></li>
+                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Age 6-12</a></li>
+                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Age 12-18</a></li>
+                            </ul>
+                        </li>
+                        <li id="menu-item-16" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-16"><a href="#">Gender</a>
+                            <ul class="sub-menu">
+                                <li id="menu-item-7" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-7"><a href="/sample-page/">Infants</a></li>
+                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Boys</a></li>
+                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Girls</a></li>
                             </ul>
                         </li>
                         <li id="menu-item-16" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-16"><a href="#">Category</a>
                             <ul class="sub-menu">
-                                <li id="menu-item-7" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-7"><a href="/sample-page/">Sub Page 1</a></li>
-                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Sub Page 2</a></li>
+                                <li id="menu-item-7" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-7"><a href="/sample-page/">T-shrts</a></li>
+                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Jeans</a></li>
+                                <li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="/sample-page-2/">Tops</a></li>
                             </ul>
                         </li>
                         
                         <li id="menu-item-15" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-15"><a href="/contact-us/">Sort By</a>
                             <ul class="sub-menu">
-                                <li id="menu-item-78" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-78"><a href="contact-us-2/our-location/">Our Location</a></li>
+                                <li id="menu-item-78" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-78"><a href="contact-us-2/our-location/">Relevance</a></li>
+                                <li id="menu-item-78" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-78"><a href="contact-us-2/our-location/">Price: Low to High</a></li>
+                                <li id="menu-item-78" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-78"><a href="contact-us-2/our-location/">Price: High to Low</a></li>
                             </ul>
                         </li>
                     </ul></div>
