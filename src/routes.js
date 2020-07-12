06f9237032bb78aafcae8629418889370eccec9b
aashishpeepra <ironhack082327@gmail.com>
2020-07-12 22:15:58 +0530

    Added File upload

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,20 @@
 import React, {Component,Suspense} from "react";
 import {Switch, Route} from 'react-router-dom';
-const Home = React.lazy(()=> import("./Containers/Home/Home"));
-const Cart = React.lazy(()=>import("./Containers/Cart/Cart"))
-const Checkout = React.lazy(()=>import("./Containers/Checkout/Checkout"))
-const SingleProduct = React.lazy(()=>import("./Containers/Single-Product/SingleProduct"))
-const Products = React.lazy(()=>import("./Containers/Products/Products"))
-const LogIn = React.lazy(()=>import("./Containers/LogIn/LogIn"))
-const Signup = React.lazy(()=>import("./Containers/Signup/Signup"))
-const LoginSignup = React.lazy(()=>import("./Containers/LoginSignup/LoginSignup"))
-const User = React.lazy(()=>import("./Containers/UserDetails/UserDetails"))
-const Admin = React.lazy(()=>import("./Containers/Admin/Admin"))
-const AdminOrders=React.lazy(()=>import("./Containers/Admin/AdminOrders/AdminOrders"))
-const AdminProducts = React.lazy(()=>import("./Containers/Admin/AdminProducts/AdminProducts"));
-
-const  EachOrder = React.lazy(()=>import("./Containers/UserDetails/EachOrder/EachOrder"))
+import Home   from "./Containers/Home/Home";
+import Cart   from "./Containers/Cart/Cart"
+import Checkout   from "./Containers/Checkout/Checkout"
+import SingleProduct   from "./Containers/Single-Product/SingleProduct"
+import Products   from "./Containers/Products/Products"
+import LogIn   from "./Containers/LogIn/LogIn"
+import Signup   from "./Containers/Signup/Signup"
+import LoginSignup   from "./Containers/LoginSignup/LoginSignup"
+import User   from "./Containers/UserDetails/UserDetails"
+import Admin   from "./Containers/Admin/Admin"
+import AdminOrders   from "./Containers/Admin/AdminOrders/AdminOrders"
+import AdminProducts   from "./Containers/Admin/AdminProducts/AdminProducts";
+import AdminProductEach   from "./Containers/Admin/AdminProducts/CreateProduct/CreateProduct";
+import  EachOrder   from "./Containers/UserDetails/EachOrder/EachOrder"
+import EachOrderAdmin   from "./Containers/Admin/AdminOrders/EachOrder/EachOrder";
 export default class Router extends Component{
     state={
 
@@ -21,8 +22,10 @@ export default class Router extends Component{
 
     render(){
         return(
-            <Suspense fallback={<h1 style={{marginTop:"80px"}}>Loading...</h1>}>
+            
             <Switch>
+                <Route path="/admin/products/:id" component={AdminProductEach}/>
+                <Route path="/admin/orders/:id" component={EachOrderAdmin}/>
                 <Route path="/admin/orders" component={AdminOrders}/>
                 <Route path="/admin/products" component={AdminProducts}/>
                 <Route path="/admin" component={Admin}/>
@@ -38,7 +41,7 @@ export default class Router extends Component{
                 <Route path="/" component={Home}/>
                 
             </Switch>
-            </Suspense>
+           
         );
     }
 }
